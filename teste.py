4848f66e67e3ee3ad5ece28bb687ee1a8fefac39
Davi <davibrilhante0102@gmail.com>
2025-05-17 20:23:19 -0300

    first

---
+++ teste.py
@@ -0,0 +1,7 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+
+model = load_model('keras_model.h5')
+print('Modelo carregado com sucesso!')
+# ... (restante do seu código)
